a793fed33952298ae7ae175c566710c9e25fa7a7
Jonathan Nogueira <jontnog@gmail.com>
2015-01-17 14:58:27 -0600

    got the pinball display working

--- Assets/Scripts/PivotController.cs
+++ Assets/Scripts/PivotController.cs
@@ -7,32 +7,72 @@ public class PivotController : MonoBehaviour {
 	public float pullback;
 	public float maxpullback;
 	bool holddown;
+	GameObject PivotEnd;
+	GameObject Dot1;
+	GameObject Dot2;
+	GameObject Dot3;
 
 	// Use this for initialization
 	void Start () {
 		holddown = false;
+		PivotEnd = transform.Find("PivotEnd").gameObject;
+		Dot1 = transform.Find("Dot1").gameObject;
+		Dot2 = transform.Find ("Dot2").gameObject;
+		Dot3 = transform.Find("Dot3").gameObject;
+		PivotEnd.renderer.enabled = false;
+		Dot1.renderer.enabled = false;
+		Dot2.renderer.enabled = false;
+		Dot3.renderer.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0))
-		{
-			Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			if(holddown == false)
-			{
-				pz.z = 0.0f;
-				transform.position = pz;
-			}
 
-			GameObject PivotEnd = transform.Find("PivotEnd").gameObject;
-			PivotEnd.transform.position = pz;
+		Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pz.z = 0f;
 
-			holddown = true;
-		}
 		if(Input.GetMouseButtonUp(0))
 		{
 			holddown = false;
+			PivotEnd.renderer.enabled = false;
+			Dot1.renderer.enabled = false;
+			Dot2.renderer.enabled = false;
+			Dot3.renderer.enabled = false;
 		}
+		if(Input.GetMouseButtonDown(0))
+		{
+			holddown = true;
+			transform.position = pz;
+		}
+
+		if(holddown == true)
+		{
+			var distance = Vector3.Distance(transform.position, pz);
+			Debug.DrawLine(transform.position, pz);
+			print("Distance: " + distance);
+			if(distance <= maxpullback)
+			{
+				Vector3 diff = transform.position - pz;
+				diff.Normalize();
+
+				float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+				PivotEnd.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 180);
+
+				PivotEnd.renderer.enabled = true;
+				Dot1.renderer.enabled = true;
+				Dot2.renderer.enabled = true;
+				Dot3.renderer.enabled = true;
+
+				PivotEnd.transform.position = pz;
+
+				diff = -(transform.position - pz);
+
+				Dot1.transform.localPosition = diff/4;
+				Dot2.transform.localPosition = diff/4 * 2;
+				Dot3.transform.localPosition = diff/4 * 3;
+			}
+		}
+
 	}
 	
 }
